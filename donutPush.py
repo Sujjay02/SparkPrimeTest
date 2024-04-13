73056324a1cc6d4c6e500dc727e638b48545856e
Sujjay02 <63737819+Sujjay02@users.noreply.github.com>
2024-04-13 13:41:05 -0400

    Create donutPush.py

---
+++ donutPush.py
@@ -0,0 +1,22 @@
+from hub import motion_sensor, port
+import runloop
+import motor_pair
+
+async def main():
+    # Pair motors on port A and B
+    motor_pair.pair(motor_pair.PAIR_1, port.C, port.D)
+
+    # Move straight at default velocity for 1 second
+    await motor_pair.move_tank_for_time(motor_pair.PAIR_1, 1000, 1000, 1000)
+
+    await runloop.sleep_ms(2000)
+
+    await motor_pair.move_tank_for_time(motor_pair.PAIR_1, 1000, -1000, 650)
+    #1000, 50, 400, 600, 550, 600, 700, 
+
+
+    motor_pair.move(motor_pair.PAIR_1,0, velocity=1000)
+
+    # cant use await on move function. Too basic
+
+runloop.run(main())
